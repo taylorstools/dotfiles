21f07bd47beacf1c2730697d23fe1e25f181dacb
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-03-22 18:07:01 -0700

    more rEFInd fixes

--- scripts/rEFInd/executable_nix_install-refind.sh
+++ scripts/rEFInd/executable_nix_install-refind.sh
@@ -3,14 +3,10 @@
 set -euo pipefail
 
 # Clone rEFInd repo
-[ -d ~/rEFInd ] || git clone https://github.com/RefindPlusRepo/rEFInd.git ~/rEFInd
-
-# Apply MR #55
-patch_file=~/scripts/rEFInd/refind-duplicate-tools.patch
-
-git -C ~/rEFInd apply "$patch_file" || true
+[ -d ~/rEFI2nd ] || git clone https://github.com/chenx-dust/rEFI2nd.git ~/rEFI2nd
 
 # Build and install rEFInd
 nix-shell ~/scripts/rEFInd/shell.nix --run "sudo refind-install"
 
+# Copy config
 sudo cp -r ~/.dotfiles/refind/. /boot/EFI/refind/
