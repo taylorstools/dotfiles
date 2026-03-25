e7fe5448492d2b93a814d4869a2f05262a13c654
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-03-24 22:48:06 -0700

    Bootstrap script

--- nixos/hosts/taylorpc/hardware-configuration.nix
+++ nixos/hosts/taylorpc/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -22,6 +22,12 @@
     { device = "/dev/disk/by-uuid/45B4-5098";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/boot";
+      fsType = "none";
+      options = [ "bind" ];
     };
 
   swapDevices = [ ];
