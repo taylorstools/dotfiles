74eeee2626c158d0a7f56035c4d36fb27008bc50
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-04-05 00:17:24 -0700

    More module separation

---
+++ nixos/modules/components/autoupgrade.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+let
+  username = "taylor";
+in
+{
+  # Automatic updates
+  system.autoUpgrade = {
+    enable = true;
+    dates = "daily";
+    flake = "${config.users.users.${username}.home}/.dotfiles/nixos";
+    flags = [
+      "--update-input" "nixpkgs"
+    ];
+    allowReboot = false;
+  };
+
+  # Garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+}
