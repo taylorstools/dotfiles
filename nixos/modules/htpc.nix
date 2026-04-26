98b14a292680424c5efeeaa55d79d27258d5dfcf
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-04-26 02:05:11 -0700

    dim overlay

--- nixos/modules/htpc.nix
+++ nixos/modules/htpc.nix
@@ -2,13 +2,17 @@
 
 {
   imports = [
+    ./components/dim-overlay
     #./components/fix-plasma-env-vars.nix
     ./components/kde-plasma.nix
     ./components/sddm-autologin.nix
     ./components/xbox-controller.nix
   ];
 
-  programs.steam.enable = true;
+  programs = {
+    steam.enable = true;
+    dim-overlay.enable = true;
+  }
 
   environment.systemPackages = with pkgs; [
     jellyfin-media-player
