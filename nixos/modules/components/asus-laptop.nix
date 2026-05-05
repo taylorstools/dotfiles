ebeb9728233e80a44d0b2047fc06f974b5f508f8
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-05-04 23:21:06 -0700

    Asus laptop

--- nixos/modules/components/asus-laptop.nix
+++ nixos/modules/components/asus-laptop.nix
@@ -8,13 +8,7 @@
   services.asusd.enable = true;
 
   # Fix touchpad not working to wake from sleep
-  systemd.services.touchpad-wakeup = {
-    description = "Disable touchpad wakeup";
-    wantedBy = [ "multi-user.target" "post-resume.target" ];
-    after = [ "post-resume.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'echo disabled > /sys/bus/i2c/devices/i2c-ASCP1A00:00/power/wakeup'";
-    };
-  };
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="i2c", KERNEL=="i2c-ASCP1A00:00", ATTR{power/wakeup}="disabled"
+  '';
 }
