b6bfbb8bc38218eb7d26563f012fc103f0da0405
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-05-04 21:18:24 -0700

    Asus laptop fixes

--- nixos/modules/components/asus-laptop.nix
+++ nixos/modules/components/asus-laptop.nix
@@ -6,4 +6,9 @@
   ];
 
   services.asusd.enable = true;
+
+  # Fix touchpad not working to wake from sleep
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="i2c", KERNEL=="i2c-ASCP1A00:00", ATTR{power/wakeup}="disabled"
+  '';
 }
