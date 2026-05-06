52e15986d893429c616774a1430424eb4a298373
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-05-05 21:45:01 -0700

    Dim overlay change

--- nixos/modules/components/dim-overlay/default.nix
+++ nixos/modules/components/dim-overlay/default.nix
@@ -23,6 +23,16 @@ in
         default = 0.90;
         description = "At or above this opacity, use fineStep instead of step.";
       };
+      ultraStep = lib.mkOption {
+        type = lib.types.float;
+        default = 0.01;
+        description = "Smallest step used when opacity is at or above ultraStepThreshold.";
+      };
+      ultraStepThreshold = lib.mkOption {
+        type = lib.types.float;
+        default = 0.96;
+        description = "At or above this opacity, use ultraStep instead of fineStep.";
+      };
       min = lib.mkOption {
         type = lib.types.float;
         default = 0.50;
@@ -44,12 +54,14 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       (pkgs.callPackage ./package.nix {
-        opacityStep              = cfg.opacity.step;
-        opacityFineStep          = cfg.opacity.fineStep;
-        opacityFineStepThreshold = cfg.opacity.fineStepThreshold;
-        opacityMin               = cfg.opacity.min;
-        opacityMax               = cfg.opacity.max;
-        opacityDefault           = cfg.opacity.default;
+        opacityStep               = cfg.opacity.step;
+        opacityFineStep           = cfg.opacity.fineStep;
+        opacityFineStepThreshold  = cfg.opacity.fineStepThreshold;
+        opacityUltraStep          = cfg.opacity.ultraStep;
+        opacityUltraStepThreshold = cfg.opacity.ultraStepThreshold;
+        opacityMin                = cfg.opacity.min;
+        opacityMax                = cfg.opacity.max;
+        opacityDefault            = cfg.opacity.default;
       })
     ];
   };
