17ebc04109065f49634a3b01fb0bdc71cd723a71
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-05-17 12:36:52 -0700

    Minor changes

--- nixos/hosts/livingroompc/configuration.nix
+++ nixos/hosts/livingroompc/configuration.nix
@@ -3,7 +3,12 @@
 {
   networking.hostName = "livingroompc";
 
+  # NVIDIA driver
+  myOptions.nvidia.mode = "proprietary";
+
   imports = [
+    ./disko.nix
+    ./hostid.nix
     ./luks-tpm-autounlock.nix
     ../../modules/htpc.nix
   ];
