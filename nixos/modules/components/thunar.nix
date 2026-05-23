8ec27d71c2a5fdafa4e24ba89d377e2c5137f266
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-05-22 19:36:03 -0700

    GTK dark mode for root

--- nixos/modules/components/thunar.nix
+++ nixos/modules/components/thunar.nix
@@ -10,6 +10,16 @@
 
   services.tumbler.enable = true;
 
+  systemd.tmpfiles.rules = let
+    gtkDark = pkgs.writeText "gtk-dark-settings.ini" ''
+      [Settings]
+      gtk-application-prefer-dark-theme=1
+    '';
+  in [
+    "L+ /root/.config/gtk-3.0/settings.ini - - - - ${gtkDark}"
+    "L+ /root/.config/gtk-4.0/settings.ini - - - - ${gtkDark}"
+  ];
+
   environment.systemPackages = with pkgs; [
     file-roller
   ];
