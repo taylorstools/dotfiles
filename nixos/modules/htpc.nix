8b6c6c9a1a892f560cc36f3b06e4d3602ebccccf
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-06-01 18:10:52 -0700

    add sunshine to default.nix

--- nixos/modules/htpc.nix
+++ nixos/modules/htpc.nix
@@ -6,7 +6,6 @@
     #./components/fix-plasma-env-vars.nix
     ./components/kde-plasma.nix
     ./components/sddm-autologin.nix
-    ./components/sunshine.nix
     ./components/ventoy-backup.nix
     ./components/xbox-controller.nix
   ];
