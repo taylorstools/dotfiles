49611df3bbd0c717c59dbf847f78f0e3eea24be6
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-06-07 21:11:40 -0700

    remove path vars

--- nixos/modules/components/kde-plasma.nix
+++ nixos/modules/components/kde-plasma.nix
@@ -4,14 +4,6 @@
   # Enable KDE
   services.desktopManager.plasma6.enable = true;
 
-  environment.sessionVariables.PATH = [
-    "/run/wrappers/bin"
-    "/home/taylor/.nix-profile/bin"
-    "/etc/profiles/per-user/taylor/bin"
-    "/nix/var/nix/profiles/default/bin"
-    "/run/current-system/sw/bin"
-  ];
-
   # Packages
   environment.systemPackages = with pkgs; [
     kdePackages.kdeconnect-kde
