9b38c272a299feaa441c4a2a33b8c8d8196b955f
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-06-07 17:01:11 -0700

    plasma broke

--- nixos/modules/components/kde-plasma.nix
+++ nixos/modules/components/kde-plasma.nix
@@ -4,6 +4,10 @@
   # Enable KDE
   services.desktopManager.plasma6.enable = true;
 
+  systemd.user.extraConfig = ''
+    DefaultEnvironment="PATH=/run/wrappers/bin:/home/taylor/.nix-profile/bin:/etc/profiles/per-user/taylor/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+  '';
+
   # Packages
   environment.systemPackages = with pkgs; [
     kdePackages.kdeconnect-kde
