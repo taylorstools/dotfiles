d47e3374faf6bc37c52aaad7bd851db0a015421b
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-06-17 18:45:29 -0700

    Add nightly reboot

--- nixos/hosts/bedroompc/configuration.nix
+++ nixos/hosts/bedroompc/configuration.nix
@@ -4,9 +4,9 @@
   networking.hostName = "bedroompc";
 
   imports = [
+    ../../modules/htpc.nix
     ./disko.nix
     ./hostid.nix
     ./luks-tpm-autounlock.nix
-    ../../modules/htpc.nix
   ];
 }
