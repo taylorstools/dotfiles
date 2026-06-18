a9fa28dd0dcd654d6040ac81cf67db9643a03bea
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-06-17 19:00:19 -0700

    lanzaboote fix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,7 +15,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/0403b4b7e8b2612657f0053a4c315e6c43eee9e6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
