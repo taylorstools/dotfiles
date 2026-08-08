612591545342753a5fb1f876ce7a3b1117bcb83f
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-08-08 15:06:08 -0700

    oops

--- nixos/modules/components/sddm-autologin.nix
+++ nixos/modules/components/sddm-autologin.nix
@@ -1,10 +1,9 @@
-{ ... }:
+{ config, ... }:
 
 let
   username = config.myOptions.user.name;
 in
 {
-  #SDDM and Plasma auto-login
   services.displayManager = {
     sddm.enable = true;
     autoLogin = {
