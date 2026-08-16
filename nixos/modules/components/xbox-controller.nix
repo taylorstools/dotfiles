d3dd2ccbd4c2d5daac4af02f29787808b0889e72
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-08-16 01:10:19 -0700

    xbox controller fix

--- nixos/modules/components/xbox-controller.nix
+++ nixos/modules/components/xbox-controller.nix
@@ -9,6 +9,8 @@
 
   services.udev.extraRules = ''
     KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
+    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="045e", ATTR{idProduct}=="02e6", TEST=="power/control", ATTR{power/control}="on"
+    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="045e", ATTR{idProduct}=="02fe", TEST=="power/control", ATTR{power/control}="on"
   '';
 
   environment.systemPackages = with pkgs; [
