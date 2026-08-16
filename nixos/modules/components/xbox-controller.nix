75d013b4663a8a7999a8a856cc7ee4f1475ed523
taylorstools <116524019+taylorstools@users.noreply.github.com>
2026-08-16 01:34:27 -0700

    xbox fix

--- nixos/modules/components/xbox-controller.nix
+++ nixos/modules/components/xbox-controller.nix
@@ -3,18 +3,13 @@
 {
   imports = [
     ./xone-idle-shutoff.nix
+    ./xone-dongle-rebind.nix
   ];
 
   hardware.xone.enable = true;
 
   services.udev.extraRules = ''
     KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
-    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="045e", ATTR{idProduct}=="02e6", TEST=="power/control", ATTR{power/control}="on"
-    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="045e", ATTR{idProduct}=="02fe", TEST=="power/control", ATTR{power/control}="on"
-  '';
-
-  boot.extraModprobeConfig = ''
-    options xone_dongle dyndbg=+p
   '';
 
   environment.systemPackages = with pkgs; [
